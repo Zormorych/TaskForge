8ffdfdc98a25283fe24dc4228cf76ef45ddde2c8
Zormorych <zormorych@gmail.com>
2026-03-01 11:22:10 +0100

    CreateTaskCommand, dto in domain added

---
+++ src/main/java/com/pdropalazn/taskforge/tasks/application/dto/CreateTaskCommand.java
@@ -0,0 +1,22 @@
+package com.pdropalazn.taskforge.tasks.application.dto;
+
+
+import com.pdropalazn.taskforge.identity.domain.model.vo.UserId;
+import com.pdropalazn.taskforge.tasks.domain.model.TaskPriority;
+import com.pdropalazn.taskforge.tasks.domain.model.TaskStatus;
+import com.pdropalazn.taskforge.tasks.domain.model.vo.TaskDescription;
+import com.pdropalazn.taskforge.tasks.domain.model.vo.TaskTitle;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+//command/dto se encarga de gestionar la entrada del caso de uso
+public record CreateTaskCommand(
+        UUID projectId,
+        TaskTitle title,
+        TaskDescription description,
+        TaskStatus status,
+        TaskPriority priority,
+        LocalDateTime dateTime,
+        UserId assigneeId
+) { }
---
+++ src/main/java/com/pdropalazn/taskforge/tasks/domain/port/in/CreateTaskUseCase.java
@@ -0,0 +1,10 @@
+package com.pdropalazn.taskforge.tasks.domain.port.in;
+
+
+//interfaz que implementa al service de aplicacion para definir como tiene que ser
+//el caso de uso
+public interface CreateTaskUseCase {
+
+
+
+}
